7bc577981ae908e46705ab11c44be54840a999a4
Rostyslaw <rostykzadyraichuk010@gmail.com>
2020-04-16 14:00:54 +0300

    add database access

--- app/src/main/java/com/example/vocation/DBOpenHelper.java
+++ app/src/main/java/com/example/vocation/DBOpenHelper.java
@@ -1,4 +1,19 @@
 package com.example.vocation;
 
-public class DBOpenHelper {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import com.readystatesoftware.sqliteasset.SQLiteAssetHelper;
+
+public class DBOpenHelper extends SQLiteAssetHelper{
+    public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "people.db";
+
+    public DBOpenHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
 }
