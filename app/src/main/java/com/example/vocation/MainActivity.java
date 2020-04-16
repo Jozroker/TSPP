a3648a4e272ce8481eeced3d165ce24193cd4677
Rostyslaw <rostykzadyraichuk010@gmail.com>
2020-04-16 17:06:52 +0300

    fix

--- app/src/main/java/com/example/vocation/MainActivity.java
+++ app/src/main/java/com/example/vocation/MainActivity.java
@@ -31,10 +31,12 @@ public class MainActivity extends AppCompatActivity {
     }
     @RequiresApi(api = Build.VERSION_CODES.N)
     public void onClickLogin(View view) {
-         if (LoginChecker.autentefication(this, editTextID.getText().toString(),
-                editTextFname.getText().toString(), editTextLname.getText().toString())) {
-             Intent intent = new Intent(getApplicationContext(),VotingsActivity.class);
-             startActivity(intent);
-         }
+//         if (LoginChecker.autentefication(this, editTextID.getText().toString(),
+//                editTextFname.getText().toString(), editTextLname.getText().toString())) {
+//             Intent intent = new Intent(getApplicationContext(),VotingsActivity.class);
+//             startActivity(intent);
+//         }
+        Intent intent = new Intent(getApplicationContext(),VotingsActivity.class);
+        startActivity(intent);
     }
 }
