4afad3c5dbe46aef7702f59387cd88d5c372933b
Rostyslaw <rostykzadyraichuk010@gmail.com>
2020-04-16 14:58:36 +0300

    removing

--- app/src/main/java/com/example/vocation/MainActivity.java
+++ app/src/main/java/com/example/vocation/MainActivity.java
@@ -1,10 +1,10 @@
 package com.example.vocation;
 
-import androidx.appcompat.app.ActionBar;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +15,16 @@ public class MainActivity extends AppCompatActivity {
         ActionBar actionBar=getSupportActionBar();
         if(actionBar !=null) {
             actionBar.hide();
+        }
+
+        try {
+            DBAccess dbAccess = DBAccess.getInstance(MainActivity.this);
+            dbAccess.open();
+            dbAccess.getValues();
+            dbAccess.close();
+
+        } catch (Exception e) {
+            System.out.println("Null");
         }
     }
 
