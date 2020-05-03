f5440aef772b6c36fecc919e5aff43e716a431f9
sashko20026 <shuravoz@gmail.com>
2020-05-03 16:06:53 +0300

    ормиофвімгфнвпмгфнимвфпфі

---
+++ app/src/main/java/com/example/vocation/VotingPage.java
@@ -0,0 +1,14 @@
+package com.example.vocation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VotingPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_voting_page);
+    }
+}
